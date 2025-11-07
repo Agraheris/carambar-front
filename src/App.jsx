b77908bba0fd0674f640a4587336e2a8d96ac35c
clement Vigouroux <clement.vigouroux@gmail.com>
2025-11-07 11:29:24 +0100

    correction pour production

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/carambar-front">
       <div className="app">
         <Navbar />
         <Routes>
